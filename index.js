9b097d96cbe9f09a6a77a827eba20768750d5f99
Kevin Zieber <zieber.kevinr@gmail.com>
2021-04-22 14:24:08 -0700

    input is now hiding and link is showing on submission

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ document.querySelector('form').addEventListener('submit', (evt) => {
   evt.preventDefault();
 
   const input = document.querySelector('#message-input');
+  const messageForm = document.querySelector('#message-form');
   const encrypted = btoa(input.value);
 
   const secret = document.querySelector('#link-input');
   secret.value = `${window.location}#${encrypted}`;
-  secret.select();
 
+  const link = document.querySelector('#message-link');
+  messageForm.classList.add('hide');
+  link.classList.remove('hide');
+
+  secret.select();
   input.value = '';
 });
